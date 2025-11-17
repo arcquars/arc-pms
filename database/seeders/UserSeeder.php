956efcfe79c878b59ee288a806e282afe1198252
arcquars <arc.quars@gmail.com>
2025-11-16 22:15:01 -0400

    crear user defaults

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
@@ -35,5 +36,43 @@ class UserSeeder extends Seeder
 
         $role = Role::create(['name' => 'super-admin']);
         $role->givePermissionTo(Permission::all());
+
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions(); 
+
+        // 3. Crear el Usuario Super Administrador
+        // Se recomienda usar la factory para un código limpio.
+        $superAdmin = User::firstOrCreate(
+            ['email' => 'superadmin@ahotel.net'],
+            [
+                'name' => 'Super Administrator',
+                // Hash::make() o bcrypt() asegura que la contraseña esté segura.
+                'password' => bcrypt('password'), 
+                'email_verified_at' => now(),
+            ]
+        );
+        // Asignar el rol creado anteriormente
+        $superAdmin->assignRole('super-admin');
+
+        // 4. Crear el Usuario Administrador
+        $admin = User::firstOrCreate(
+            ['email' => 'admin@ahotel.net'],
+            [
+                'name' => 'Administrator',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $admin->assignRole('admin');
+        
+        // 5. Crear el Usuario Cliente (Customer)
+        $customer = User::firstOrCreate(
+            ['email' => 'customer@ahotel.net'],
+            [
+                'name' => 'Default Customer',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+        $customer->assignRole('customer');
     }
 }
